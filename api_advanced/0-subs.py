f59655dc2f4601f838e3162a014ed2c49f5de2bb
madol-abraham <m.madol@alustudent.com>
2023-11-11 09:49:20 +0000

    updated

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
-'''python script for api'''
+'''
+python script for api
+'''
 
-        import json
-        import requests
-
-
-        def number_of_subscribers(subreddit):
-
-            """
-            A function that queries the Reddit API for a given subreddit and
-            returns the total number of subscribers. Returns 0 if the subreddit is invalid
-            """
-            url = f"https://www.reddit.com/r/{subreddit}/about.json"
-            headers ={'User- Agent': 'mkm user agent 1.1'}
-            response = requests.get(url, headers=headers, allow_redirects=False)
+import requests
 
 
-            if response.status_code !=200:
-                return 0
-            return response.json().get('data').get('subcribers')
+def number_of_subscribers(subreddit):
+            
+     """
+     A function that queries the Reddit API for a given subreddit and 
+     returns the total number of subscribers. Returns 0 if the subreddit is invalid
+     """
+     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+     headers ={'User-Agent': 'Hp/5(microsoft windows 11 pro)'}
+     response = requests.get(url, headers=headers, allow_redirects=False)
+            
+            
+     if response.status_code !=200:
+        return 0
+     return response.json().get('data').get('subcribers')
